4255cf86207e780319bd01fead3198353d332202
Azar <azar.barfipour@gmail.com>
2021-09-05 23:02:36 -0700

    creatWebsite

---
+++ js/javascript.js
@@ -0,0 +1,12 @@
+$(function () {
+    $("#button1").click(function () {
+        $("#topmenu ul").slideToggle();
+    });
+
+
+    //$(window).resize(function () {
+    //    if ($("#cats").css("width") != "300px") {
+    //        $("#cats .submenu").slideUp();
+    //    }
+    //});
+});
